dab78b51ec32f7f14f54debbbae360cb2e960715
Baber Khan <bbrkhnn@outlook.com>
2024-08-14 23:08:31 +0100

    Use https instead of http for API calls

--- src/components/MoreDetails.jsx
+++ src/components/MoreDetails.jsx
@@ -4,82 +4,88 @@ import { Button } from "@mui/material";
 import ArrowCircleLeftIcon from "@mui/icons-material/ArrowCircleLeft";
 
 export default function MoreDetails({ location, setPage }) {
-  const [todaysWeather, setTodaysWeather] = useState([]);
-  const [currentLocation, setCurrentLocation] = useState("");
-  const [currentDate, setCurrentDate] = useState("");
+    const [todaysWeather, setTodaysWeather] = useState([]);
+    const [currentLocation, setCurrentLocation] = useState("");
+    const [currentDate, setCurrentDate] = useState("");
 
-  function setLocation(response) {
-    const country = response.data.location.country;
-    const region = response.data.location.region;
-    const name = response.data.location.name;
+    function setLocation(response) {
+        const country = response.data.location.country;
+        const region = response.data.location.region;
+        const name = response.data.location.name;
 
-    setCurrentLocation({ name, region, country });
-  }
+        setCurrentLocation({ name, region, country });
+    }
 
-  function setDate(response) {
-    const date = response.data.forecast.forecastday[0].date;
-    const [year, month, day] = date.split("-");
+    function setDate(response) {
+        const date = response.data.forecast.forecastday[0].date;
+        const [year, month, day] = date.split("-");
 
-    const lookupObject = {
-      1: "January",
-      2: "February",
-      3: "March",
-      4: "April",
-      5: "May",
-      6: "June",
-      7: "July",
-      8: "August",
-      9: "September",
-      10: "October",
-      11: "November",
-      12: "December",
-    };
+        const lookupObject = {
+            1: "January",
+            2: "February",
+            3: "March",
+            4: "April",
+            5: "May",
+            6: "June",
+            7: "July",
+            8: "August",
+            9: "September",
+            10: "October",
+            11: "November",
+            12: "December",
+        };
 
-    setCurrentDate(`${Number(day)} ${lookupObject[Number(month)]} ${year}`);
-  }
+        setCurrentDate(`${Number(day)} ${lookupObject[Number(month)]} ${year}`);
+    }
 
-  function getTime(dateTime) {
-    return dateTime.split(" ")[1];
-  }
+    function getTime(dateTime) {
+        return dateTime.split(" ")[1];
+    }
 
-  useEffect(() => {
-    axios
-      .get(
-        `http://api.weatherapi.com/v1/forecast.json?q=${location}&days=3&key=${
-          import.meta.env.VITE_API_KEY
-        }`
-      )
-      .then((response) => {
-        setTodaysWeather(response.data.forecast.forecastday[0].hour);
-        setLocation(response);
-        setDate(response);
-      });
-  }, [location]);
+    useEffect(() => {
+        axios
+            .get(
+                `https://api.weatherapi.com/v1/forecast.json?q=${location}&days=3&key=${
+                    import.meta.env.VITE_API_KEY
+                }`
+            )
+            .then((response) => {
+                setTodaysWeather(response.data.forecast.forecastday[0].hour);
+                setLocation(response);
+                setDate(response);
+            });
+    }, [location]);
 
-  return (
-    <section className="more-details">
-      <div className="button">
-        <Button
-          onClick={() => setPage("weather-data")}
-          startIcon={<ArrowCircleLeftIcon />}
-        >
-          Go Back
-        </Button>
-      </div>
-      <div className="location">
-        <h2>{`${currentLocation.name}, ${currentLocation.region}`}</h2>
-        <h3>{currentLocation.country}</h3>
-        <p className="date">{currentDate}</p>
-      </div>
-      <div>
-        {todaysWeather.map((today) => (
-          <div key={today.time_epoch} className="detail">
-            <h3>{getTime(today.time)}</h3>
-            <img src={today.condition?.icon} alt={today.condition?.text} />
-            <p>{today.temp_c} &deg;C</p>
-          </div>
-        ))}
-      </div>
-    </section>
-  );
+    return (
+        <section className="more-details">
+            <div className="button">
+                <Button
+                    onClick={() => setPage("weather-data")}
+                    startIcon={<ArrowCircleLeftIcon />}
+                >
+                    Go Back
+                </Button>
+            </div>
+            <div className="location">
+                <h2>{`${currentLocation.name}, ${currentLocation.region}`}</h2>
+                <h3>{currentLocation.country}</h3>
+                <p className="date">{currentDate}</p>
+            </div>
+            <div>
+                {todaysWeather.map((today) => (
+                    <div
+                        key={today.time_epoch}
+                        className="detail"
+                    >
+                        <h3>{getTime(today.time)}</h3>
+                        <img
+                            src={today.condition?.icon}
+                            alt={today.condition?.text}
+                        />
+                        <p>{today.temp_c} &deg;C</p>
+                    </div>
+                ))}
+            </div>
+        </section>
+    );
 }
--- src/components/WeatherData.jsx
+++ src/components/WeatherData.jsx
@@ -5,100 +5,112 @@ import { Button } from "@mui/material";
 import ArrowCircleLeftIcon from "@mui/icons-material/ArrowCircleLeft";
 
 export default function WeatherData({ location, setPage }) {
-  const [weather, setWeather] = useState({});
-  const [isLoading, setIsLoading] = useState(false);
-  const [currentLocation, setCurrentLocation] = useState("");
+    const [weather, setWeather] = useState({});
+    const [isLoading, setIsLoading] = useState(false);
+    const [currentLocation, setCurrentLocation] = useState("");
 
-  function setLocation(response) {
-    const country = response.data.location.country;
-    const region = response.data.location.region;
-    const name = response.data.location.name;
+    function setLocation(response) {
+        const country = response.data.location.country;
+        const region = response.data.location.region;
+        const name = response.data.location.name;
 
-    setCurrentLocation({ name, region, country });
-  }
+        setCurrentLocation({ name, region, country });
+    }
 
-  useEffect(() => {
-    setIsLoading(true);
-    axios
-      .get(
-        `http://api.weatherapi.com/v1/forecast.json?q=${location}&days=3&key=${
-          import.meta.env.VITE_API_KEY
-        }`
-      )
-      .then((response) => {
-        setWeather(response.data);
-        setLocation(response);
-        setIsLoading(false);
-      });
-  }, [location]);
+    useEffect(() => {
+        setIsLoading(true);
+        axios
+            .get(
+                `https://api.weatherapi.com/v1/forecast.json?q=${location}&days=3&key=${
+                    import.meta.env.VITE_API_KEY
+                }`
+            )
+            .then((response) => {
+                setWeather(response.data);
+                setLocation(response);
+                setIsLoading(false);
+            });
+    }, [location]);
 
-  return (
-    <section className="weather-data">
-      <div className="button">
-        <Button
-          onClick={() => setPage("search")}
-          startIcon={<ArrowCircleLeftIcon />}
-        >
-          Go Back
-        </Button>
-      </div>
-      {isLoading && <div className="loading">Loading...</div>}
-      {!isLoading && (
-        <>
-          <div className="current-weather">
-            <div className="location">
-              <h2>{`${currentLocation.name}, ${currentLocation.region}`}</h2>
-              <h3>{currentLocation.country}</h3>
+    return (
+        <section className="weather-data">
+            <div className="button">
+                <Button
+                    onClick={() => setPage("search")}
+                    startIcon={<ArrowCircleLeftIcon />}
+                >
+                    Go Back
+                </Button>
             </div>
-            <div className="bigger">
-              <img
-                src={weather.current?.condition.icon}
-                alt={weather.current?.condition.text}
-              />
-              <p className="temperature">{weather.current?.temp_c} &deg;C</p>
-            </div>
-            <p>{weather.current?.condition.text}</p>
-            <p>Feels like {weather.current?.feelslike_c} &deg;C</p>
-            <p>UV Index: {weather.current?.uv}</p>
-            <p>{weather.current?.humidity}% humidity</p>
-            <p>Wind Speed: {weather.current?.wind_mph} mph</p>
-            <Button
-              variant="outlined"
-              onClick={() => {
-                setPage("more-details");
-              }}
-            >
-              More Details
-            </Button>
-          </div>
-          <div className="forecast">
-            <h3>Forecast</h3>
-            <div>
-              {weather.forecast?.forecastday.map((current, index) => {
-                return (
-                  <div key={index}>
-                    <img
-                      src={current.day.condition.icon}
-                      alt={current.day.condition.text}
-                    />
-                    <p>
-                      <span>{current.day.mintemp_c} &deg;C</span> /
-                      <span> {current.day.maxtemp_c} &deg;C</span>
-                    </p>
-                    <p>
-                      {index === 0
-                        ? "Today"
-                        : index === 1
-                        ? "Tomorrow"
-                        : "Day After"}
-                    </p>
-                  </div>
-                );
-              })}
-            </div>
-          </div>
-        </>
-      )}
-    </section>
-  );
+            {isLoading && <div className="loading">Loading...</div>}
+            {!isLoading && (
+                <>
+                    <div className="current-weather">
+                        <div className="location">
+                            <h2>{`${currentLocation.name}, ${currentLocation.region}`}</h2>
+                            <h3>{currentLocation.country}</h3>
+                        </div>
+                        <div className="bigger">
+                            <img
+                                src={weather.current?.condition.icon}
+                                alt={weather.current?.condition.text}
+                            />
+                            <p className="temperature">
+                                {weather.current?.temp_c} &deg;C
+                            </p>
+                        </div>
+                        <p>{weather.current?.condition.text}</p>
+                        <p>Feels like {weather.current?.feelslike_c} &deg;C</p>
+                        <p>UV Index: {weather.current?.uv}</p>
+                        <p>{weather.current?.humidity}% humidity</p>
+                        <p>Wind Speed: {weather.current?.wind_mph} mph</p>
+                        <Button
+                            variant="outlined"
+                            onClick={() => {
+                                setPage("more-details");
+                            }}
+                        >
+                            More Details
+                        </Button>
+                    </div>
+                    <div className="forecast">
+                        <h3>Forecast</h3>
+                        <div>
+                            {weather.forecast?.forecastday.map(
+                                (current, index) => {
+                                    return (
+                                        <div key={index}>
+                                            <img
+                                                src={current.day.condition.icon}
+                                                alt={current.day.condition.text}
+                                            />
+                                            <p>
+                                                <span>
+                                                    {current.day.mintemp_c}{" "}
+                                                    &deg;C
+                                                </span>{" "}
+                                                /
+                                                <span>
+                                                    {" "}
+                                                    {current.day.maxtemp_c}{" "}
+                                                    &deg;C
+                                                </span>
+                                            </p>
+                                            <p>
+                                                {index === 0
+                                                    ? "Today"
+                                                    : index === 1
+                                                    ? "Tomorrow"
+                                                    : "Day After"}
+                                            </p>
+                                        </div>
+                                    );
+                                }
+                            )}
+                        </div>
+                    </div>
+                </>
+            )}
+        </section>
+    );
 }
